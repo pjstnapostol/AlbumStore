de2f82e5f56dbe16903b7217c690d5cf5df4a719
Paul Justin Apostol <apostol.pj4@gmail.com>
2023-03-16 16:06:37 +0800

    AlbumStore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
